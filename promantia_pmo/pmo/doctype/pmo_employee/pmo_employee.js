e72590484e244cb4fc75e912e9525395ab72cca7
Hari Madhavan <hari.madhavan@promantia.com>
2023-11-12 22:36:48 +0530

    Added Employee Details

---
+++ promantia_pmo/pmo/doctype/pmo_employee/pmo_employee.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Promantia Business Solutions and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('PMO_Employee', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ promantia_pmo/pmo/doctype/pmo_employee/pmo_employee.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Promantia Business Solutions and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class PMO_Employee(Document):
+	pass
