c926eb25e93f41a1c317ab2b1255f29365b668c0
Hari Madhavan <hari.madhavan@promantia.com>
2023-11-12 18:49:24 +0530

    Added initial version of Project based on Redmine

---
+++ promantia_pmo/pmo/doctype/pmo_project/pmo_project.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Promantia Business Solutions and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+class PMO_Project(NestedSet):
+	pass
---
+++ promantia_pmo/pmo/doctype/pmo_project/test_pmo_project.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Promantia Business Solutions and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPMO_Project(FrappeTestCase):
+	pass
