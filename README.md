616c4b590a753406d2db5eddb3590a6eae465023
Hari Madhavan <hari.madhavan@promantia.com>
2023-11-12 11:32:53 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Promantia Project Management App
+
+Imports Project and HR Data and matches costs vs estimates and costs vs income
+
+#### License
+
+MIT
---
+++ promantia_pmo/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Promantia Project Management App",
+			"type": "module",
+			"label": _("Promantia Project Management App")
+		}
+	]
